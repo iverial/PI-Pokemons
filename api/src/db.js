e2ad75d5d0b5d2884c23fc611fdc0ade1ed32849
Ivan <100495676+iverial@users.noreply.github.com>
2022-08-10 20:04:10 -0300

    fix base de datos

--- api/src/db.js
+++ api/src/db.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const { Sequelize } = require('sequelize');
 const fs = require('fs');
 const path = require('path');
-const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME } = process.env;
+const { DB_USER, DB_PASSWORD, DB_HOST,DB_NAME} = process.env;
 let sequelize =
   process.env.NODE_ENV === "production"
     ? new Sequelize({
@@ -28,7 +28,7 @@ let sequelize =
         ssl: true,
       })
     : new Sequelize(
-        `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/@${DB_NAME}`,
+        `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`,
         { logging: false, native: false }
       );
 const basename = path.basename(__filename);
