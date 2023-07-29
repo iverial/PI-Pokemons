4a68f865f45aed437cc823d4547e3b07150da808
Ivan <100495676+iverial@users.noreply.github.com>
2023-07-29 20:25:02 -0300

    cambios

--- api/src/db.js
+++ api/src/db.js
@@ -60,5 +60,5 @@ Tipo.belongsToMany(Pokemon, {through: "Pokemontipo"})
 
 module.exports = {
   ...sequelize.models, // para poder importar los modelos así: const { Product, User } = require('./db.js');
-  conn: sequelize,     // para importart la conexión { conn } = require('./db.js');
+  conn: sequelize,     // para importart la conexión { conn } = require('./db.js'); sa
 };
